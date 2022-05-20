afa497968ebc730be67d3794838f1c878e5953c9
zeyad <zeyad_khaled@outlook.com>
2022-05-20 14:56:52 +0200

    clean examples

--- examples/cluster/KMeans.py
+++ examples/cluster/KMeans.py
@@ -12,37 +12,29 @@ def main():
     X, _ = make_blobs(n_samples = 1000, centers = clusters,
     								cluster_std = 0.60)
 
-    # # After training the model, We store the
-    # # coordinates for the cluster centers
-    ms = KMeans(5)
-    ms.fit(X)
+    # mlalgo
+    ms = KMeans(5).fit(X)
     cluster_centers = ms.centers
-    # cluster_centers = ms.cluster_centers_
-    # Finally We plot the data points
-    # and centroids in a 3D graph.
-    print(ms.centers, ms.score(np.array([[2,1.5],[4.5,4]])))
-    print(ms.inertia)
+    print("Score: ", ms.score(np.array([[2,1.5],[4.5,4]])))
+    print("Inertia: ", ms.inertia)
+    
+    # Finally We plot the data points and centroids
     fig = plt.figure()
 
     ax = fig.add_subplot(111)
 
     ax.scatter(X[:, 0], X[:, 1],marker ='o',c=ms.labels)
-    # ax.scatter(X[:, 0], X[:, 1],marker ='o',c=ms.labels_)
     ax.scatter(cluster_centers[:, 0], cluster_centers[:, 1], marker ='x', color ='red',
     		s = 300, linewidth = 5, zorder = 1)
 
     plt.show()
-    # # After training the model, We store the
-    # # coordinates for the cluster centers
     
-    ms = KM(5,init="random")
-    ms.fit(X)
-    print(ms.cluster_centers_, ms.score(np.array([[2,1.5],[4.5,4]])))
-    print(ms.inertia_)
+    # sklearn
+    ms = KM(5,init="random").fit(X)
     cluster_centers = ms.cluster_centers_
+    print("Score: ", ms.score(np.array([[2,1.5],[4.5,4]])))
+    print("Inertia: ", ms.inertia_)
 
-    # Finally We plot the data points
-    # and centroids in a 3D graph.
     fig = plt.figure()
 
     ax = fig.add_subplot(111)
